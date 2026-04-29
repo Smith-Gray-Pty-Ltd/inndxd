b4e1b856d6197c77e92e8cb00afd451aa17d8b93
James A Birrell Gray <james@jamesbirrellgray.com>
2026-04-29 23:17:04 +1000

    phase0: 0-7 add missing __init__.py to core domain subpackage

---
+++ packages/inndxd-core/src/inndxd_core/domain/__init__.py
@@ -0,0 +1,12 @@
+from inndxd_core.domain.brief import BriefCreate, BriefRead
+from inndxd_core.domain.data_item import DataItemCreate, DataItemRead
+from inndxd_core.domain.project import ProjectCreate, ProjectRead
+
+__all__ = [
+    "ProjectCreate",
+    "ProjectRead",
+    "BriefCreate",
+    "BriefRead",
+    "DataItemCreate",
+    "DataItemRead",
+]
